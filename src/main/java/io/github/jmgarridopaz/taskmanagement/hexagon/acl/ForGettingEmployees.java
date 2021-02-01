93a6e3c11fcde41c5b02f60c9d95b74b5671c5e4
jmgarridopaz <44113936+jmgarridopaz@users.noreply.github.com>
2021-02-01 09:09:25 +0100

    Update ForGettingEmployees.java

--- src/main/java/io/github/jmgarridopaz/taskmanagement/hexagon/acl/ForGettingEmployees.java
+++ src/main/java/io/github/jmgarridopaz/taskmanagement/hexagon/acl/ForGettingEmployees.java
@@ -1,6 +1,8 @@
 package io.github.jmgarridopaz.taskmanagement.hexagon.acl;
 
-
+/*
+ * Driven Port
+ */
 public interface ForGettingEmployees {
 
 	public Employee getEmployeeFromId ( String employeeId );
